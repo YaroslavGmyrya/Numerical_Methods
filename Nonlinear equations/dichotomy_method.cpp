f53a9ddb026facfd534cd7cbe1dce17a82e72936
YaroslavGmyrya <jjaked25@gmail.com>
2025-07-04 19:25:39 +0700

    Add README and dichotomy method

---
+++ Nonlinear equations/dichotomy_method.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+double f(double x){
+    return (x-1) * pow((x-2), 2) * pow((x-3), 3);
+}
+
+
+double dichotomy_method(double a, double b, double eps){
+
+    if(f(a) * f(b) > 0)
+        return NAN;
+
+    double c;
+
+    while(fabs(a - b) > eps){
+
+        c = (a + b) / 2;
+
+        f(a) * f(c) < 0 ? b = c : a = c;
+    
+    }
+
+    return c;
+}
+
+int main(){
+    double a, b, eps;
+
+    std::cout << "Enter the left limit of the range: ";
+    std::cin >> a;
+
+    std::cout << "Enter the right limit of the range: ";
+    std::cin >> b;
+
+    std::cout << "Enter the accuracy: ";
+    std::cin >> eps;
+
+    std::cout << "Result: " << dichotomy_method(a, b, eps) << std::endl;
+
+    return 0;
+}
